3f899fe408ba72acb0211c53561aea3dda0bd7e1
chc0331 <ss01003@naver.com>
2022-04-26 01:37:46 +0900

    Implement logic for change profile image - add glide library - add firestore logic for store image at Firebase server.

--- ToyProject/app/src/main/java/com/example/myapplication/toyproject/presenter/chatting/profile/owner/OwnerProfileFragment.kt
+++ ToyProject/app/src/main/java/com/example/myapplication/toyproject/presenter/chatting/profile/owner/OwnerProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication.toyproject.presenter.chatting.profile.owner
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.myapplication.toyproject.R
 import com.example.myapplication.toyproject.core.BaseFragment
 import com.example.myapplication.toyproject.databinding.FragmentOwnerProfileBinding
@@ -19,6 +23,17 @@ class OwnerProfileFragment : BaseFragment() {
             OwnerProfileViewModel::class.java
         )
     }
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val uri = it.data!!.data
+                binding?.profileImage.background = null
+                Glide.with(activity!!).load(uri)
+                    .override(200, 200)
+                    .into(binding?.profileImage)
+                vm?.imageUri = uri.toString()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +54,18 @@ class OwnerProfileFragment : BaseFragment() {
                 editProfilePassword.setText(it.password)
             }
         })
+        binding.profileImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startForResult.launch(intent)
+        }
+        binding.changeProfileOk.setOnClickListener {
+            vm?.changeUserProfile(
+                binding.editProfileName.text.toString(),
+                binding.editProfilePhone.text.toString(),
+                binding.editProfilePassword.text.toString()
+            )
+        }
     }
 
     override fun onResume() {
--- ToyProject/app/src/main/java/com/example/myapplication/toyproject/presenter/chatting/profile/owner/OwnerProfileViewModel.kt
+++ ToyProject/app/src/main/java/com/example/myapplication/toyproject/presenter/chatting/profile/owner/OwnerProfileViewModel.kt
@@ -1,20 +1,25 @@
 package com.example.myapplication.toyproject.presenter.chatting.profile.owner
 
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.data.model.User
 import com.example.myapplication.data.repository.UserDataRepository
 import com.example.myapplication.toyproject.core.FireBaseViewModel
+import com.google.firebase.storage.FirebaseStorage
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OwnerProfileViewModel(private val repository: UserDataRepository) : FireBaseViewModel() {
     private val TAG: String = OwnerProfileViewModel::class.java.simpleName
+    private val storage: FirebaseStorage = FirebaseStorage.getInstance()
     private var _user: MutableLiveData<User> = MutableLiveData()
     val user: LiveData<User>
         get() = _user
+    var imageUri: String = ""
 
     fun updateUserProfile() {
-        Log.d("heec.choi", getUserUUID())
         repository.getUserByUuid(getUserUUID()).doOnError {
             Log.d(TAG, "getUserByUUID onError")
         }.doOnComplete {
@@ -25,4 +30,24 @@ class OwnerProfileViewModel(private val repository: UserDataRepository) : FireBa
         }.subscribe()
     }
 
+    fun changeUserProfile(name: String, phone: String, password: String) {
+        Log.d(
+            "heec.choi",
+            "imageUri : $imageUri, name : $name, phone : $phone, password : $password"
+        )
+    }
+
+    fun updateImageDatabase(uri: Uri?) {
+        uri?.apply {
+            val fileName = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+            storage.reference.child("image").child(fileName)
+                .putFile(uri)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val downloadUrl = task.result
+                        val url = downloadUrl.toString()
+                    }
+                }
+        }
+    }
 }
